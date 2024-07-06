9a00fe059ef31c28bfbbb32399eb37eac967d3a9
angel luna <angelstivengarcialuna@gmail.com>
2024-07-05 21:29:53 -0500

    arreglando cositas

--- src/sockets/sockets.js
+++ src/sockets/sockets.js
@@ -1,40 +1,61 @@
 const Band = require("../../models/band");
 const Bands = require("../../models/bands");
 const { io } = require("../index");
+
 const bands = new Bands();
 console.log('nit server');
-bands.addBand(new Band("queen"))
-bands.addBand(new Band("Mago de Oz"))
-bands.addBand(new Band("Metalica"))
-bands.addBand(new Band("los bitles"))
-console.log(bands)
+bands.addBand(new Band("queen"));
+bands.addBand(new Band("Mago de Oz"));
+bands.addBand(new Band("Metalica"));
+bands.addBand(new Band("los bitles"));
+console.log(bands);
 
 io.on('connection', client => {
     console.log("Cliente conectado");
-    client.emit('active-bands', bands.getBans())
+    client.emit('active-bands', bands.getBans());
+
     client.on('disconnect', () => {
         console.log("Cliente desconectado");
     });
 
     client.on('mensaje', (p) => {
-        console.log("Mensaje !! " + p.nombre);
-        io.emit("mensaje", { admin: "Nuevo Mensaje" });
+        if (p && p.nombre) {
+            console.log("Mensaje !! " + p.nombre);
+            io.emit("mensaje", { admin: "Nuevo Mensaje" });
+        } else {
+            console.log("Mensaje recibido sin nombre");
+        }
     });
+
     client.on('emitir-mensaje', (p) => {
-      client.broadcast('nuevo-mensaje', p )
+        client.broadcast.emit('nuevo-mensaje', p);
     });
+
     client.on('vote-band', (p) => {
-        bands.voteBand(p.id);
-        io.emit('active-bands', bands.getBans())
-      });
+        if (p && p.id) {
+            bands.voteBand(p.id);
+            io.emit('active-bands', bands.getBans());
+        } else {
+            console.log("Vote received without id");
+        }
+    });
+
     client.on('add-band', (p) => {
-        const NewBand = new Band(p.name)
-        bands.addBand(NewBand);
-        io.emit('active-bands', bands.getBans())
-        });
+        if (p && p.name) {
+            const newBand = new Band(p.name);
+            bands.addBand(newBand);
+            io.emit('active-bands', bands.getBans());
+        } else {
+            console.log("Add band received without name");
+        }
+    });
 
     client.on('delete-band', (p) => {
+        if (p && p.id) {
             bands.deleteBands(p.id);
-            io.emit('active-bands', bands.getBans())
-            });
+            io.emit('active-bands', bands.getBans());
+        } else {
+            console.log("Delete band received without id");
+        }
+    });
 });
